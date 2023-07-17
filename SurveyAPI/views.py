51ad8502d781eb204bfe5a8ba743e2f282548e0f
Yigal Nirenberg <nirigor@gmail.com>
2023-07-17 19:10:44 +0300

    17071910

--- SurveyAPI/views.py
+++ SurveyAPI/views.py
@@ -53,31 +53,12 @@ def participantApi(request):
         data['BFNeuroticismScore'] = statistics.mean([int(data['BF1']), int(data['BF2']), int(data['BF3'])])
 
         # Calculate TKI
-        data['TKAccommodatingScore'] = \
-            (data['TK1'] == 'B') + (data['TK3'] == 'B') + (data['TK4'] == 'B') + (data['TK11'] == 'B') + \
-            (data['TK15'] == 'A') + (data['TK16'] == 'A') + (data['TK18'] == 'A') + (data['TK21'] == 'A') + \
-            (data['TK24'] == 'A') + (data['TK25'] == 'B') + (data['TK27'] == 'B') + (data['TK30'] == 'A')
-
-        data['TKCompetingScore'] = \
-            (data['TK3'] == 'A') + (data['TK6'] == 'B') + (data['TK8'] == 'A') + (data['TK9'] == 'B') + \
-            (data['TK10'] == 'A') + (data['TK13'] == 'B') + (data['TK14'] == 'B') + (data['TK16'] == 'B') + \
-            (data['TK17'] == 'A') + (data['TK22'] == 'B') + (data['TK25'] == 'A') + (data['TK28'] == 'A')
-
-        data['TKAvoidingScore'] = \
-            (data['TK1'] == 'A') + (data['TK5'] == 'B') + (data['TK6'] == 'A') + (data['TK7'] == 'A') + \
-            (data['TK9'] == 'A') + (data['TK12'] == 'A') + (data['TK15'] == 'B') + (data['TK17'] == 'B') + \
-            (data['TK19'] == 'B') + (data['TK23'] == 'B') + (data['TK27'] == 'A') + (data['TK29'] == 'B')
-
-        data['TKCompromisingScore'] = \
-            (data['TK2'] == 'A') + (data['TK4'] == 'A') + (data['TK7'] == 'B') + (data['TK10'] == 'B') + \
-            (data['TK12'] == 'B') + (data['TK13'] == 'A') + (data['TK18'] == 'B') + (data['TK20'] == 'B') + \
-            (data['TK22'] == 'A') + (data['TK24'] == 'B') + (data['TK26'] == 'A') + (data['TK29'] == 'A')
-
-        data['TKCollaboratingScore'] = \
-            (data['TK2'] == 'B') + (data['TK5'] == 'A') + (data['TK8'] == 'B') + (data['TK11'] == 'A') + \
-            (data['TK14'] == 'A') + (data['TK19'] == 'A') + (data['TK20'] == 'A') + (data['TK21'] == 'B') + \
-            (data['TK23'] == 'A') + (data['TK26'] == 'B') + (data['TK28'] == 'B') + (data['TK30'] == 'B')
-
+        data['TKAccommodatingScore'] = (data['TK2'] == 'B') + (data['TK3'] == 'B') + (data['TK7'] == 'B') + (data['TK10'] == 'A')
+        data['TKCompetingScore'] = (data['TK2'] == 'A') + (data['TK5'] == 'B') + (data['TK6'] == 'A') + (data['TK9'] == 'B')
+        data['TKAvoidingScore'] = (data['TK4'] == 'B') + (data['TK5'] == 'A') + (data['TK8'] == 'A') + (data['TK10'] == 'B')
+        data['TKCompromisingScore'] = (data['TK1'] == 'A') + (data['TK3'] == 'A') + (data['TK8'] == 'B') + (data['TK9'] == 'A')
+        data['TKCollaboratingScore'] = (data['TK1'] == 'B') + (data['TK4'] == 'A') + (data['TK6'] == 'B') + (data['TK7'] == 'A')
+        
         feedback = {
             'BFOpennessScore' : data['BFOpennessScore'],
             'BFAgreeablenessScore' : data['BFAgreeablenessScore'],
